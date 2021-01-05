a4872d7ad336ab1e45016a518a547e7795d668de
SysPoe <panddplayz@outlook.com>
2021-01-05 19:10:02 +1000

    Added client

---
+++ client/src/Client.java
@@ -0,0 +1,78 @@
+import com.eclipsesource.json.Json;
+
+import java.io.*;
+import java.net.*;
+
+public class Client extends Thread {
+    String ip;
+    String password;
+    PrintWriter printOut;
+    InetAddress host;
+    Socket socket;
+    BufferedReader reader;
+    Client(String ip, String password) {
+        this.ip=ip;
+        this.password = password;
+    }
+
+    @Override
+    public void run() {
+        try{
+            InstaMessage.loading.setVisible(true);
+            InstaMessage.Incorrect.setText("Incorrect password or IP address.");
+            host = InetAddress.getByName(ip);
+            socket = new Socket( host, 1456);
+            System.out.println("Connecting to remote server...");
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            printOut = new PrintWriter(socket.getOutputStream());
+            printOut.println(Json.object().add("requestType", "verify").add("content", password));
+            System.out.println("Sending value: \"" + Json.object().add("requestType", "verify").add("content", password) + "\" to server");
+            printOut.flush();
+            String messageIn = reader.readLine();
+            if(messageIn.equals("denied")) {
+                InstaMessage.loading.setVisible(false);
+                InstaMessage.Incorrect.setVisible(true);
+                System.out.println("Access to remote server denied.");
+                socket.close();
+            }
+            else if (messageIn.equals("accept")) {
+                printOut.println(Json.object().add("requestType", "registerUsername").add("content" , InstaMessage.Nickname.getText()));
+                System.out.println("Sending value: \"" + Json.object().add("requestType", "registerUsername").add("content" , InstaMessage.Nickname.getText()) + "\" to server");
+                printOut.flush();
+                System.out.println("Registering nickname...");
+                messageIn = reader.readLine();
+
+                if (messageIn.equals("accept")) {
+                    InstaMessage.Title.setVisible(false);
+                    InstaMessage.IP.setVisible(false);
+                    InstaMessage.Password.setVisible(false);
+                    InstaMessage.Incorrect.setVisible(false);
+                    InstaMessage.loading.setVisible(false);
+                    System.out.println("connected.");
+                }
+                else if (messageIn.equals("denied")) {
+                    InstaMessage.Incorrect.setVisible(true);
+                    InstaMessage.loading.setVisible(false);
+                    InstaMessage.Incorrect.setText("That nickname is taken!");
+                    socket.close();
+                }
+                else {
+                    InstaMessage.loading.setVisible(false);
+                    System.out.println("Server returned unexpected value: " + messageIn);
+                    socket.close();
+                }
+            }
+            else {
+                InstaMessage.loading.setVisible(false);
+                System.out.println("Server returned unexpected value: " + messageIn);
+                socket.close();
+            }
+        } catch(IOException ioe) {
+            System.out.println("Failed to connect.");
+            InstaMessage.Incorrect.setVisible(true);
+            InstaMessage.Incorrect.setText("Incorrect password or IP address.");
+            InstaMessage.loading.setVisible(false);
+        }
+    }
+}
---
+++ client/src/InstaMessage.java
@@ -0,0 +1,117 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.*;
+
+public class InstaMessage extends JFrame{
+    static GraphicsEnvironment graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    static GraphicsDevice device = graphics.getDefaultScreenDevice();
+    static Border blackLine = BorderFactory.createLineBorder(Color.black);
+    static JLabel Title = new JLabel("InstaMessage");
+    static JLabel Incorrect = new JLabel("Incorrect password or IP address.");
+    static JTextField IP = new JTextField("Enter IP address");
+    static JTextField Nickname = new JTextField("Enter Nickname");
+    static JTextField Password = new JTextField("Enter password");
+    static JButton close = new JButton("X");
+    static Icon loadingGif;
+    static JLabel loading;
+    static Client chatClient;
+
+    InstaMessage() {
+        device.setFullScreenWindow(this);
+        setLayout(null);
+        setTitle("InstaMessage");
+
+        loadingGif = new ImageIcon(this.getClass().getResource("connect.gif"));
+
+        close.setBounds(getWidth()-50,0,50,50);
+        close.addActionListener(e -> {
+            setVisible(false);
+            System.exit(0);
+        });
+        add(close);
+
+
+        IP.setBounds(getWidth()/2-100, 60, 200, 20);
+        IP.setHorizontalAlignment(JTextField.CENTER);
+        IP.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (IP.getText().equals("Enter IP address")) IP.setText("");
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (IP.getText().equals("")) IP.setText("Enter IP address");
+            }
+        });
+        add(IP);
+
+        Nickname.setBounds(getWidth()/2-100, 90, 200, 20);
+        Nickname.setHorizontalAlignment(JTextField.CENTER);
+        Nickname.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (Nickname.getText().equals("Enter Nickname")) Nickname.setText("");
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (Nickname.getText().equals("")) Nickname.setText("Enter Nickname");
+            }
+        });
+        add(Nickname);
+
+
+        Password.setBounds(getWidth()/2-100, 120, 200, 20);
+        Password.setHorizontalAlignment(JTextField.CENTER);
+        Password.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (Password.getText().equals("Enter password")) Password.setText("");
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (Password.getText().equals("")) Password.setText("Enter password");
+            }
+        });
+        Password.addActionListener(e -> {
+            chatClient = new Client(IP.getText(), Password.getText());
+            chatClient.start();
+        });
+        add(Password);
+
+        Incorrect.setBounds(getWidth()/2-100, 150, 200, 20);
+        Incorrect.setHorizontalAlignment(JTextField.CENTER);
+        Incorrect.setForeground(new Color(255, 0 ,0));
+        Incorrect.setVisible(false);
+        add(Incorrect);
+
+        Title.setBounds(getWidth()/2-getWidth()/4, 0, getWidth()/2, 60);
+        Title.setFont(getFont(getWidth()/48));
+        Title.setHorizontalAlignment(JLabel.CENTER);
+        Title.setVerticalAlignment(JLabel.CENTER);
+        add(Title);
+
+        loading = new JLabel(loadingGif);
+        loading.setBounds(getWidth()/2-50, 220, 100,100);
+        loading.setVisible(false);
+        add(loading);
+
+        setVisible(true);
+        setVisible(false);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        InstaMessage instaMessage = new InstaMessage();
+    }
+    public static String formatForJLabel(String orig)
+    {
+        return "<html>" + orig.replaceAll("\n", "<br>");
+    }
+    public static Font getFont(int size)
+    {
+        return new Font("Sans-Serif", Font.PLAIN, size);
+    }
+}
