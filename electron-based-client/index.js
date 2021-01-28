1ace2d83fe5c0c8ae4314c5dbed09c9bc8528c7f
SysPoe <panddplayz@outlook.com>
2021-01-28 19:43:02 +1000

    Updated Electron client & Server

---
+++ electron-based-client/index.js
@@ -0,0 +1,70 @@
+let ip;
+let username;
+let password;
+
+let websocket;
+let uuid = 0;
+let previousChat = "";
+
+function connect() {
+    ip = document.getElementById("ip").value;
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+
+    websocket = new WebSocket("ws://"+ip+":63439");
+    websocket.onopen = onOpen;
+    websocket.onmessage = onMessage;
+    websocket.onerror = onError;
+
+    document.getElementById("web-send-message").addEventListener("keyup", function(e) {
+        if(e.key === "Enter") {
+            e.preventDefault();
+            sendMessage();
+        }
+    });
+
+    document.getElementById("loading").style.display="block";
+    
+}
+function onOpen() {
+    const tosendJSON = {type: "validate", password: password, username: username};
+    const tosend = JSON.stringify(tosendJSON);
+    console.log("Connected to server... authenticating");
+    document.getElementById("loading").style.display="none";
+    websocket.send(tosend);
+}
+function onMessage(e) {
+    const message = JSON.parse(e.data);
+    if (message.type === "token") {
+        uuid = message.content;
+        console.log("verified, token is: "+uuid);
+        document.getElementById("web-error").innerHTML = "";
+        document.getElementById("inputs").style.display = "none";
+        document.getElementById("web-chat-screen").style.display = "block";
+    } else if (message.type === "error") {
+        console.log("Error: "+message.content);
+        document.getElementById("web-error").innerHTML = message.content;
+        if(message.content === "Invalid username!") {
+            document.getElementById("inputs").style.display = "block";
+            document.getElementById("web-chat-screen").style.display = "none";
+        }
+    } else if (message.type === "client") {
+        console.log("Message from server: " + message.type + " " + message.content);
+    } else if (message.type === "chat") {
+        document.getElementById("web-chat").innerHTML = message.content.replace(/\n/g, "<br />");
+        if (message.content.replace(/\n/g, "<br />") !== previousChat) {
+            document.getElementById("web-chat").scrollTop = document.getElementById("web-chat").scrollHeight;
+        }
+        previousChat = message.content.replace(/\n/g, "<br />");
+    }
+    else {
+        console.log("Recieved from server: "+e.data);
+    }
+}
+function onError() {
+    document.getElementById("web-error").innerHTML = "Unable to connect to host: server not found.";
+}
+function sendMessage() {
+    websocket.send(JSON.stringify({type: "chat", content: document.getElementById("web-send-message").value, token: uuid}));
+    document.getElementById("web-send-message").value = "";
+}
